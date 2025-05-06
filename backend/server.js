08f93187ebc6bc743ca59faef3240a9b5c38f7cd
HuynhMinhThangV <22130251@st.hcmuaf.edu.vn>
2025-05-06 20:38:01 +0700

    connect db

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,20 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
-dotenv.config(); // Load environment variables
-
+import connectDB from "./config/db.js"
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Sample route
+
+connectDB()
 app.get("/", (req, res) => {
   res.send("Backend is running...");
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
